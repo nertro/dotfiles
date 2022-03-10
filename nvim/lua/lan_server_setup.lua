69b8e1cd11b767190b2bc46f81e3a3fee2d21155
Mena Jacobs <menalisa.jacobs@gmail.com>
2022-03-10 11:07:01 +0100

    fix(nvim-lsp): remove dummy code

--- nvim/lua/lan_server_setup.lua
+++ nvim/lua/lan_server_setup.lua
@@ -60,10 +60,6 @@ cmp.setup.cmdline(':', {
 
   -- Setup lspconfig.
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
--- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-require('lspconfig')['<YOUR_LSP_SERVER>'].setup {
-  capabilities = capabilities
-}
 
 local opts = { noremap=true, silent=true }
 vim.api.nvim_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
